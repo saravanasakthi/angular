b1ace0cb04745080b16ebbb7e467253f810bf267
saravana <41249153+saravanasakthi@users.noreply.github.com>
2019-01-06 15:17:27 +0530

    Add files via upload

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+import { HEROES } from '../mock.hero';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+  selectedHero: Hero = {
+    id: 1,
+    name: 'WindStorm',
+  };
+
+  heroes = HEROES;
+
+  onSelect(hero: Hero):void{
+    this.selectedHero = hero;
+  }
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
